7d6e33a0cf658ba1631ce436716a73dfad60f3c6
Sivasish Praharaj <sivasish48@gmail.com>
2025-03-25 23:57:20 +0530

    initiation and installing tailwind v4

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='bg-red-500'>
+      <h1>Hello world</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
